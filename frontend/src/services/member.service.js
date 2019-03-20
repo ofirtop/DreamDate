43330f3e049dc7b413141bde1a686cee41504327
ofirtop <ofirtopaz@gmail.com>
2019-03-20 19:50:50 +0200

    something

--- frontend/src/services/member.service.js
+++ frontend/src/services/member.service.js
@@ -1,5 +1,10 @@
+const BASE_URL = (process.env.NODE_ENV !== 'development')
+    ? '/'
+    : '//localhost:3003/';
+    
 export default {
     query,
+    removeItem
 }
 // var members = [{_id: '111', name: 'Puki', age: 23}, {_id: '222', name: 'Muki', age: 33}, {_id: '333',name: 'Kuki', age: 43}]
 // function query() {
@@ -8,4 +13,9 @@ export default {
 function query() {
     return axios.get('/user')
         .then(res => members = res.data);
+}
+
+function removeItem(itemId) {
+    return axios.delete(`${BASE_URL}toy/${itemId}`)
+        .then(() => itemId)
 }
