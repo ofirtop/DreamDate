6041a6adc11a87a3e154341be0cac6f54c9869de
Hagar Rachman <orrrachman@gmail.com>
2019-03-20 15:43:43 +0200

    added sass, add home component style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,29 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <nav id="nav" class="flex items-center">
+      <router-link to="/">
+        <img src="@/assets/img/logo.png" alt="logo" class="logo">
+      </router-link>
       <router-link to="/about">About</router-link>
-    </div>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@import "@/sass/_variables.scss";
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
 }
 #nav {
-  padding: 30px;
   a {
     font-weight: bold;
     color: #2c3e50;
     &.router-link-exact-active {
-      color: #42b983;
+      color: $color_primary;
     }
   }
+}
+.logo {
+  width: 40px;
 }
 </style>
