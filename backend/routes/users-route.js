e0e7b361002fbae24ebb5e2696120dfced5b19ed
ofirtop <ofirtopaz@gmail.com>
2019-03-25 16:37:54 +0200

    fix get single

--- backend/routes/users-route.js
+++ backend/routes/users-route.js
@@ -46,7 +46,7 @@ function addUserRoutes(app) {
     //GET single
     app.get('/user/:userId', (req, res) => {
         let userId = req.params.userId;
-        userService.getById(userId, req.session.loggedInUser)
+        userService.getByIdTemp(userId, req.session.loggedInUser)
             .then(user => res.json(user))
     })
 
