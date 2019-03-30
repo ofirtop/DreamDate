08544d6838e865a39a3382f83828af891ec7045f
Tatiana Podolsky <podolskaya.tatiana@gmail.com>
2019-03-30 12:39:12 +0300

    merge

--- backend/public/service-worker.js
+++ backend/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5387c5bdb1d352f0245d7173f9c90052.js"
+  "/precache-manifest.6e82dd0ec02c858abbf7d03def70dede.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "frontend"});
