982fb1ce1d7bbe37ea6325d3cf762e65ca0bad44
ofirtop <ofirtopaz@gmail.com>
2019-04-06 12:38:04 +0300

    MatchAd look change

--- frontend/src/components/MatchAd.vue
+++ frontend/src/components/MatchAd.vue
@@ -1,33 +1,18 @@
 <template>
   <section class="match-cmp">
     <div v-if="currentMatchMember" class="centered animated delay-2s fadeInUp">
-      <div class="flex content-center">
+      <div class="newMatch ">It's a Match!</div>
+      <div class="newMatch subtitle mt-2">You and {{currentMatchMember.name}} have liked each other.</div>
+      <div class="mt-5 flex content-center">
         <img class="match " :src="loggedInUser.mainImage">
         <img class="match " :src="currentMatchMember.mainImage">
       </div>
-      <div class="newMatch ">You have New Match !</div>
-      <div class="flex space-between newMatch">
+      <div class="flex space-around newMatch actionBar mt-5">
         <div class="underline pointer" @click="goToProfilePage">See Profile</div>
         <div class="underline pointer" @click="closeAnimation">Later</div>
       </div>
     </div>
   </section>
-  <!-- <section class="match-cmp">
-    <div v-if="currentMatchMember" class="centered animated delay-2s fadeInUp">
-      <div class="flex">
-        <img class="match animated delay-2s fadeInUp" :src="loggedInUser.mainImage">
-        <img class="match animated delay-2s fadeInUp" :src="currentMatchMember.mainImage">
-      </div>
-      <div class="newMatch animated delay-2s fadeInUp">You have New Match !</div>
-      <div class="flex space-between newMatch">
-        <div
-          class="underline cursor animated delay-1s fadeInUp"
-          @click="goToProfilePage"
-        >See Profile</div>
-        <div class="underline cursor animated delay-1s fadeInUp" @click="closeAnimation">Later</div>
-      </div>
-    </div>
-  </section> -->
 </template>
 
 <script>
@@ -69,37 +54,44 @@ export default {
 @import "../sass/_variables.scss";
 
 .centered {
-  // position: fixed;
-  // top: 50%;
-  // left: 50%;
-  // transform: translate(-50%, -50%);
-  // padding: 50px;
-  // background-color:lightgrey;
     position: fixed;
     top: 15%;
-    left: 26%;
+    left: 17.5%;
     -webkit-transform: translate(-50%, -50%);
-    /* transform: translate(-50%, -50%); */
     padding: 50px;
-    background: rgba(0, 0, 0, 0.1); 
-    width: 50vw;
-    height: 35vh;
+    background: rgba(28, 28, 28, 0.8);
+    width: 65vw;
+    height: 50vh;
     
 }
 .match {
   width: 200px;
   height: 200px;
   border-radius: 50%;
-  border: 5px solid white;
-  box-shadow: 0px 0px 32px 32px #fff;
+  border: 3px solid white;
+  margin: 0 25px;
+  // border: 5px solid white;
+  // box-shadow: 0px 0px 32px 32px #fff;
 }
 .newMatch {
-  margin-top: 40px;
+  text-align: center;
   font-family: "Dancing Script", cursive;
-  font-size: 40px;
+  font-size: 160px;
   font-weight: 900;
   color: white;
   text-shadow: 4px 1px 17px #a44d11;
+}
+.newMatch.actionBar{
+  font-size: 37px;
+  font-family: inherit;  
+  font-weight: 400;
+}
+.newMatch.subtitle {
+  font-family: inherit;
+  font-size: 37px;
+  text-align: center;
+  font-weight: 400;
+  color: white;
 }
 .pointer {
   cursor: pointer;
